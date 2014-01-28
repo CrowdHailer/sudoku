f5628713b856f77ddf03153a184a3616b51a58cf
Peter Saxton <peterhsaxton@gmail.com>
2014-01-28 11:50:34 +0000

    Added filter to get unsolved cells

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,3 +1,11 @@
 class Grid
-	
+	def initialize
+		@cells = [:cells]*81
+	end
+
+	attr_reader :cells
+
+	def unsolved_cells 
+		cells
+	end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,5 +1,13 @@
 require './lib/grid'
 
 describe Grid do
-	
+	let(:grid) { Grid.new }
+
+	it 'should have 81 cells' do
+		expect(grid.cells.count).to eq(81)
+	end
+
+	it 'should have 81 unsolved cells' do
+		expect(grid.unsolved_cells.count).to eq(81)
+	end
 end
