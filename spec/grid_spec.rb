cc2540adda313459f846bbc4ac605ead9e95ab74
Peter Saxton <peterhsaxton@gmail.com>
2014-01-28 15:04:25 +0000

    reduced test grid to 16 cells

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,14 +2,14 @@ require './lib/grid'
 
 describe Grid do
 	context 'Empty Grid' do
-		let(:grid) { Grid.new 3 }
+		let(:grid) { Grid.new 2 }
 
 		it 'should have n to the 4th cells' do
-			expect(grid.cells.count).to eq(81)
+			expect(grid.cells.count).to eq(16)
 		end
 
 		it 'should have 16 unsolved cells' do
-			expect(grid.unsolved_cells.count).to eq(81)
+			expect(grid.unsolved_cells.count).to eq(16)
 		end
 
 		it 'should not be solved' do
